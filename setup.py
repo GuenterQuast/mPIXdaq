c4d93c569fcfb4068c5c381835dc8e253401b937
Guenter Quast <guenter.quast@online.de>
2025-07-09 20:37:09 +0200

    pixet.ini from AppDataDir

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
+# save ADVACAM libraries and miniPIX EDU firmware
 setup(
     name='advacam',
     packages=['advacam'],
     package_dir={'advacam': 'advacam'},
-    package_data={'advacam': ['*.so', '*.ini']},
+    package_data={'advacam': ['*.so', '*.ini','factory/*']},
     description='Provides ADVACAM pixet API to python users',
     version='1.8.3',
     url='https://yo.com',
@@ -12,3 +13,4 @@ setup(
     author_email='',
     keywords=['pip', 'advacam']
     )
+
