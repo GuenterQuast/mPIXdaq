d6d8affaa91a05c3a03c287c398485a7a5af8979
Guenter Quast <guenter.quast@online.de>
2025-07-12 11:45:00 +0200

    set LD_LIBRARY_PATH before starting Python

--- run_mPIXdaq.py
+++ run_mPIXdaq.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S LD_LIBRARY_PATH=. python3
+# on some platforms, the currnt directory needs to be in LD_LIBRARY_PATH
+# so that all neccesary C-libraries are found by the pypixet interface
+#  the "addition -S LD_LIBRARY_PATH=." does the trick ... 
 
 from mpixdaq import mpixdaq
 mpixdaq.run()
