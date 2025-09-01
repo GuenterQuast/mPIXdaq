faf660ecb96d0270cd8d74cfae80b36800efe981
Guenter Quast <guenter.quast@online.de>
2025-09-01 16:40:15 +0200

    fixed typoes in doc string

--- mpixdaq/advacam_arm64/__init__.py
+++ mpixdaq/advacam_arm64/__init__.py
@@ -1,7 +1,7 @@
 """ADVACAM pixet1.8.3 library for armhf
 
 Note: arm version also depends on library zest.so, which
- is only found by linker if the current directory is in $LD_LIBRARY_PAHT:
+ is only found by linker if the current directory is in $LD_LIBRARY_PATH:
  export LD_LIBRARY_PATH='.' before starting the python script
 
 """
@@ -12,7 +12,7 @@ import sys
 cur_file_dir = os.path.dirname(os.path.realpath(__file__))
 # add current file directory so that pypixet.so is found by python
 sys.path.append(cur_file_dir)
-# add to paht so that pypixet.so finds its dependencies
+# add to path so that pypixet.so finds its dependencies
 os.chdir(cur_file_dir)
 
 # load every symbols of pypixet into upper module
--- mpixdaq/advacam_win64/__init__.py
+++ mpixdaq/advacam_win64/__init__.py
@@ -1,8 +1,6 @@
 """ADVACAM pixet1.8.3 library for armhf
 
-Note: arm version also depends on library zest.so, which
- is only found by linker if the current directory is in $LD_LIBRARY_PAHT:
- export LD_LIBRARY_PATH='.' before starting the python script
+Note: Windows version needs local directory in library path 
 
 """
 
@@ -12,8 +10,8 @@ import sys
 cur_file_dir = os.path.dirname(os.path.realpath(__file__))
 # add current file directory so that pypixet.so is found by python
 sys.path.append(cur_file_dir)
-# add to paht so that pypixet.so finds its dependencies
+# add to path so that pypixet.so finds its dependencies
 os.chdir(cur_file_dir)
 
-# load every symbols of pypixet into upper module
+# load every symbol of pypixet into upper module
 from pypixet import *
