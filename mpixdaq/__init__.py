97fe010a63bfef178e9ea373164c1354096fd6a6
Guenter Quast <guenter.quast@online.de>
2025-09-27 14:58:46 +0200

    removed import of all submodules from __init__.py

--- mpixdaq/__init__.py
+++ mpixdaq/__init__.py
@@ -9,8 +9,5 @@ import sys
 # Import version info
 from ._version_info import _get_version_string
 
-# import of package modules (needed for Python 3.7 under Windows)
-from .mpixdaq import *
-
 # and set version
 __version__ = _get_version_string()
