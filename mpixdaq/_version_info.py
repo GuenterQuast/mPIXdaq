362c763a5c31cac7401afedd08dcf9787ef3540d
Guenter Quast <guenter.quast@online.de>
2025-12-13 16:02:12 +0100

    v1.0.0rc0

--- mpixdaq/_version_info.py
+++ mpixdaq/_version_info.py
@@ -9,7 +9,7 @@
 major = 1
 minor = 0
 revision = 0
-suffix = "beta"  # for suffixes such as 'rc' or 'beta' or 'alpha'
+suffix = "rc0"  # for suffixes such as 'rc' or 'beta' or 'alpha'
 
 
 def _get_version_tuple():
